ec09d8fd0e1d9ba1eabc741bcf737207e8c6a650
大鱼 <160@3688.com>
2018-04-14 17:39:17 +0800

    c

--- src/js/1.js
+++ src/js/1.js
@@ -1,21 +1,32 @@
 $(function () {
+    //退出登录
+    $('#logout').click(function(){
+        $('.right .logout').toggleClass('hide')
+        $('.right .log_up').toggleClass('hide')
+    })
+    // $('aside').tinyscrollbar();
+
+    //树形菜单
     $('li:has(ol)').addClass('expand').click(function () {
         $(this).toggleClass('fold').children('ol').toggle(200);
     });
-    $('li').click(function (ev) {
+    $('li,.Ser').click(function (ev) {
         // 阻止冒泡
         ev.stopPropagation();
     });
-    // $('.list li a').click(()=>{
-    // 	this.css('background','#92c609')
+
+    // $('.list li a').click(function(){
+    // 	$(this).toggleClass('.green')
     // })
+    //显示树形菜单
     $('#btn').click(function(){
         $('#bottom aside').toggleClass('hide')
     })
+    //文本框显示与否
+    $('.Sea').click(function(){
+        $(this).addClass('Rch').blur(function(){
+            $(this).removeClass('Rch')
+        })
+    })
 
 })
-// (function () {
-//     new Vue({
-//         el:'#bottom'
-//     })
-// })()
